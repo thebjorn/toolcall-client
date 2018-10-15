c0a0950ce8502d1bd598b8a8950d241b9596cee8
thebjorn <bp@datakortet.no>
2018-10-15 09:17:20 +0200

    toolcall server

--- toolcall_client/appsettings.py
+++ toolcall_client/appsettings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# TOOLCALL_SERVER_URL = 'https://afr.norsktest.no/toolcall/'
-TOOLCALL_SERVER_URL = 'https://toolcall-server.eu.ngrok.io/toolcall/'
+TOOLCALL_SERVER_URL = 'https://afr.norsktest.no/toolcall/'
+# TOOLCALL_SERVER_URL = 'https://toolcall-server.eu.ngrok.io/toolcall/'
 
 TOOLCALL_FETCH_START_DATA_URL = TOOLCALL_SERVER_URL + 'fetch-token/'
 RESULT_TOKEN_URL = TOOLCALL_SERVER_URL + 'result/'
